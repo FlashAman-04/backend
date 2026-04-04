301b2accd7fd328e7e95b907c00d9aec900a95a3
Aman Singh <believeraman6321@gmail.com>
2026-04-04 14:37:42 +0530

    Checks to registration api complete

--- 05_authentication/src/controllers/auth.controllers.js
+++ 05_authentication/src/controllers/auth.controllers.js
@@ -1,18 +1,58 @@
-const userModel=require('../models/user.model');
-const jwt=require('jsonwebtoken');
+const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 // const cookieParser=require('cookie-parser');
 
 
-async function registerUser(req,res){
-    const {username, email, password} =req.body;
-    
+async function registerUser(req, res) {
+    const { username, email, password } = req.body;
+
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "All Fields are required.."
+        })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+
+    const existingUser = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
+
+    // console.log(existingUser);
+
+
+    if (existingUser) {
+        if (existingUser.username == username) {
+            return res.status(400).json({
+                message: "Username already exists"
+            })
+        }
+        else {
+            return res.status(400).json({
+                message: "Email already exists"
+            })
+        }
+    }
+
     const user = await userModel.create({
-        username, email, password
+        username,
+        email,
+        password: hashedPassword
     });
 
 
-    const token=jwt.sign({
-        id:user._id
+
+
+
+    const token = jwt.sign({
+        id: user._id
     }, process.env.JWT_SECRET);
 
 
@@ -20,11 +60,11 @@ async function registerUser(req,res){
 
 
     res.status(201).json({
-        message:"User registered Successfully",
+        message: "User registered Successfully",
         user
     });
 
 };
 
 
-module.exports={ registerUser };
+module.exports = { registerUser };
--- 05_authentication/src/models/user.model.js
+++ 05_authentication/src/models/user.model.js
@@ -3,7 +3,11 @@ const mongoose=require('mongoose');
 
 const userSchema=new mongoose.Schema({
     // a bit more complex schema can be created with more properties and validations but for now,taking simplicity in mind we are keeping it basic
-    username:String,
+    username:{
+        type:String,
+        unique:true,
+        required:true
+    },
     email:String,
     password:String
 });
